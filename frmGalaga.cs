a2be7782d12459a798a34e654c9499e9e2833a60
Francisco Elias <franeliasbv@gmail.com>
2024-04-14 17:58:22 -0300

    Comentarios completos

--- frmGalaga.cs
+++ frmGalaga.cs
@@ -29,10 +29,13 @@ namespace pryEliasFrancisco
             
         }
 
+        //Función para mover la nave con las flechas 
         void Mover_Con_Flechas()
         {
+            //Si la bandera es true ingresa 
             if (derecha == true)
             {
+                //Condición que utilizo para que la nave haga tope en el costado del formulario
                 if (pbNave.Left < 550)
                 {
                     pbNave.Left += 30;
@@ -41,6 +44,7 @@ namespace pryEliasFrancisco
 
             if (izquierda == true)
             {
+                //Condición que utilizo para que la nave haga tope en el costado del formulario
                 if (pbNave.Left > 10)
                 {
                     pbNave.Left -= 30;
@@ -48,29 +52,36 @@ namespace pryEliasFrancisco
             }
         }
 
+        //Función para el movimiento del enemigo
         void Movimiento_Enemigo()
         {
+            //Creo una instancia de la clase random para generar números aleatorios donde saldrán los enemigos 
             Random enemigo = new Random();
 
-            int x, y;
+            //Variables para almacenar los posiciones aleatorias donde se moverán los enemigos 
+            int posicionUno, posicionDos;
 
             if (pbEnemigo.Top >= 700)
             {
-                x = enemigo.Next(0, 584);
-                pbEnemigo.Location = new Point(x, 0);
+                //Asigno a posicionUno el número aleatorio(Que representa la nueva posición horizontal del enemigo)
+                posicionUno = enemigo.Next(0, 584);
+                pbEnemigo.Location = new Point(posicionUno, 0);
             }
             if (pbEnemigoDos.Top >= 500)
             {
-                y = enemigo.Next(0, 584);
-                pbEnemigoDos .Location = new Point(y, 0);
+                //Asigno a posicionDos el número aleatorio(Que representa la nueva posición horizontal del segundo enemigo)
+                posicionDos = enemigo.Next(0, 584);
+                pbEnemigoDos .Location = new Point(posicionDos, 0);
             }
             else 
             {
+                //Incremento la posición vertical
                 pbEnemigo.Top += 15;
                 pbEnemigoDos.Top += 10;
             }
         }
         
+        //Función para crear el pictureBox que contiene el Misil
         void Agregar_Misil()
         {
             PictureBox pbMisil = new PictureBox();
@@ -80,47 +91,63 @@ namespace pryEliasFrancisco
             pbMisil.Tag = "Misil";
             pbMisil.Left = pbNave.Left + 8;
             pbMisil.Top = pbNave.Top - 50;
+            //Añado el control PictureBox al formulario
             this.Controls.Add(pbMisil);
+            //Hago que el pictureBox creado este al frente de los otros controles en el formulario y se vea 
             pbMisil.BringToFront();
         }
 
+        //Función para el movimiento del misil 
         void Movimiento_Misil()
         {
-            foreach (Control x in this.Controls)
+            //Con este bucle foreach recorro todos los controles que estan contenidos en esto formulario 
+            foreach (Control controlActual in this.Controls)
             {
-                if (x is PictureBox && x.Tag == "Misil")
+                //En esta condición ingresa si el control es un pictureBox y su propiedad Tag es "Misil", la cual le asigne al pictureBox del Misil 
+                if (controlActual is PictureBox && controlActual.Tag == "Misil")
                 {
-                    x.Top -= 20;
-                    if(x.Top < 10)
+                    //Si el control es el pictureBox hago que se mueva
+                    controlActual.Top -= 20;
+                    if(controlActual.Top < 10)
                     {
-                        this.Controls.Remove(x);
+                        this.Controls.Remove(controlActual);
                     }
                 }
             }
         }
         
+        //Función en la cual si se intersectan los pictureBox del misil y enemigo desaparece el enemigo
         void Resultado_Juego()
         {
+            //Con este bucle foreach recorro todos los controles que estan contenidos en esto formulario 
             foreach (Control misil in this.Controls)
             {
+                //Con este bucle foreach recorro todos los controles que estan contenidos en esto formulario 
                 foreach (Control enemigo in this.Controls)
                 {
-                    if(misil is PictureBox && misil.Tag == "Misil")
+                    //En esta condición ingresa si el control es un pictureBox y su propiedad Tag es "Misil", la cual le asigne al pictureBox del Misil
+                    if (misil is PictureBox && misil.Tag == "Misil")
                     {
+                        //En esta condición ingresa si el control es un pictureBox y su propiedad Tag es "Enemigo", la cual le asigne a los pictureBox de los Enemigos
                         if (enemigo is PictureBox && enemigo.Tag == "Enemigo")
                         {
+                            //Si los límites de los dos pictureBox se intersectan significa que colisionan, entonces ingresa 
                             if (misil.Bounds.IntersectsWith(enemigo.Bounds))
                             {
+                                //Muevo al enemigo hacia arriba simulando la muerte del mismo 
                                 enemigo.Top -= 1000;
-
+                                
+                                //Incremento un punto en el puntaje del jugador
                                 puntaje = puntaje + 1;
 
+                                //Muestro en un label el puntaje 
                                 lblPuntaje.Text = "Puntaje: " + puntaje;
                             }
                         }
                     }
                 }
             }
+            //Si la nave choca con alguno de los enemigos ingresa y hago que finalice el juego 
             if(pbNave.Bounds.IntersectsWith(pbEnemigoDos.Bounds) || pbNave.Bounds.IntersectsWith(pbEnemigo.Bounds))
             {
                 timerNave.Stop();
@@ -131,7 +158,7 @@ namespace pryEliasFrancisco
 
                 DialogResult resultado = MessageBox.Show("Su puntaje fue: " + puntaje + "\n¿Desea volver al menú principal?", "Game Over", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
 
-                // Verificar la respuesta del usuario
+                //Verifico la respuesta del usuario
                 if (resultado == DialogResult.OK)
                 {
                     frmMenuPrincipal frmMenuPrincipal = new frmMenuPrincipal();
@@ -142,18 +169,22 @@ namespace pryEliasFrancisco
             }
         }
 
+        //Procedimiento del evento KeyDown(Presionar una tecla) para mover con las flechas la nave y disparar con espacio
         private void frmGalaga_KeyDown(object sender, KeyEventArgs e)
         {
+            //Si presiona la flecha derecha pongo la bandera en true para poder generar el movimiento que cree en la función Mover_Con_Flechas
             if (e.KeyCode == Keys.Right)
             {
                 derecha = true;
             }
 
+            //Si presiona la flecha izquierda pongo la bandera en true para poder generar el movimiento que cree en la función Mover_Con_Flechas
             if (e.KeyCode == Keys.Left)
             {
                 izquierda = true;
             }
 
+            //Si presiona el espacio pongo la bandera en true para poder generar el movimiento que cree en la función Movimiento_Misil
             if (e.KeyCode == Keys.Space)
             {
                 espacio = true;
@@ -182,8 +213,10 @@ namespace pryEliasFrancisco
             Movimiento_Misil();
         }
 
+        //Procedimiento del evento KeyUp(Cuando se suelta la tecla) cambio las banderas a false para que no dispare ni se mueva la nave sola 
         private void frmGalaga_KeyUp(object sender, KeyEventArgs e)
         {
+            //Cuando se sueltan las teclas cambio las banderas a false para detener las acciones que realizan dichas teclas
             if (e.KeyCode == Keys.Right)
             {
                 derecha = false;
