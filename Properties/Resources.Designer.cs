cb50b3899845d56151481ed1a6631c3efd2823ec
Francisco Elias <franeliasbv@gmail.com>
2024-04-18 14:53:21 -0300

    Acomodado el misil

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -133,6 +133,16 @@ namespace pryEliasFrancisco.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap MisilSinFondo2MasChica {
+            get {
+                object obj = ResourceManager.GetObject("MisilSinFondo2MasChica", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap nave {
             get {
                 object obj = ResourceManager.GetObject("nave", resourceCulture);
