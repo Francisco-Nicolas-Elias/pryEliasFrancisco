879d657e36bf6cfb0b292b6b1e9c4f92c7f275a4
Francisco Elias <franeliasbv@gmail.com>
2024-05-13 20:23:07 -0300

    Comence a conectar la base de datos para guardar el score y el nombre del jugador

---
+++ clsBaseDatos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using MySql.Data.MySqlClient;
+
+namespace pryEliasFrancisco
+{
+    internal class clsBaseDatos
+    {
+        public void ConectarBD()
+        {
+            try
+            {
+                string cadenaConexion = "";
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
